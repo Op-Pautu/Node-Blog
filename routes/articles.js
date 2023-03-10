b493d1e7f5c237da74634075534d38c47ca8392c
Op Pautu <pautuop@gmail.com>
2023-03-10 22:43:34 +0530

    Add a partial view page to add new posts

--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("You're on the articles page");
+router.get("/new", (req, res) => {
+  res.render("articles/new");
 });
 
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Create");
+});
 module.exports = router;
